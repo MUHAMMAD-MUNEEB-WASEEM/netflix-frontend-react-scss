a7c5072b2badd45b6b904f649c3c2e500d84bb20
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-30 17:06:50 +0500

    Watch page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.scss';
 import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
 
 function App() {
   return (
     <div className="app">
-      <Home/>
+      <Watch/>
     </div>
   );
 }
